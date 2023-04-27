7e9a53ee536be6ac463099b6cf2ff1f90476a7b1
julianetaplus <juliangarcia.email@gmail.com>
2023-04-27 12:57:56 -0300

    block key

--- next.config.js
+++ next.config.js
@@ -3,10 +3,7 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  env: {
-    mapbox_key:
-      "pk.eyJ1Ijoiam9vbGlhbiIsImEiOiJjbGd3ZXA3Y3owYjdjM2pxdDV6MDk4NHRhIn0.jBaNMthKcmkofIU5tgYCrg",
-  },
+  //
 };
 
 module.exports = nextConfig;
