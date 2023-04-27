e01af1c171d8b8a60b51ef27b13a72a0d938409e
julianetaplus <juliangarcia.email@gmail.com>
2023-04-27 13:25:08 -0300

    active link styling

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  //
+  env: {
+    mapbox_key:
+      "pk.eyJ1Ijoiam9vbGlhbiIsImEiOiJjbGd6YjgxYWcwYWZpM3FveHoyNWRmY2t4In0.dGvhZK7fqrao1Sjd5BYaxg",
+  },
 };
 
 module.exports = nextConfig;
